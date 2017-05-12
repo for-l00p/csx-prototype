eeb14554b5ceb8eaae702ee7a61f3d021254511c
Jimmy Huynh <jkchuynh829@gmail.com>
2017-05-12 14:50:45 -0700

    added arrows for repeater and index

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-const express = require('express');
-const path = require('path');
-const app = express();
 
-app.use(express.static(path.join(__dirname, './csx-gitbook/csx-static')));
-
-app.listen(3000);
-=======
 const express = require('express')
 const path = require('path');
 const app = express();
@@ -14,4 +6,3 @@ const app = express();
 app.use(express.static(path.join(__dirname)));
 
 app.listen(3000, () => console.log('Express server listening on 3000'))
->>>>>>> cleanup
