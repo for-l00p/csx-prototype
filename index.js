79095dce7e818253cb15fd2c134a5d0c346f114b
Jimmy Huynh <jkchuynh829@gmail.com>
2017-05-11 21:36:04 -0700

    applyfuncs template

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, './csx/static/')));
+app.use(express.static(path.join(__dirname)));
 
 app.listen(3000, () => console.log('Express server listening on 3000'))
